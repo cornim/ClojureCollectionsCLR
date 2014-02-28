695b234fbf32ad2e049cf8ce4be32629c1a869f0
Cornelius Mund <cornelius.mund@d-fine.de>
2014-02-28 15:41:50 +0100

    Updated documention in IPersistentList to C# style.

--- ClojureCollectionsCLR/IPersistentList.cs
+++ ClojureCollectionsCLR/IPersistentList.cs
@@ -2,46 +2,56 @@
 
 namespace ClojureCollectionsCLR
 {
-    /**Interface for a persistent (immutable) list.
-    * All methods are guaranteed to leave the object itself unmodified and only
-    * return modified copies of the original.
-    *
-    * @author Dr. Cornelius Mund
-    *
-    * @param <T> Type of items to be contained in the list. */
+    /// <summary>
+    /// Interface for a persistent (immutable) list.
+    /// All methods are guaranteed to leave the object itself unmodified and only
+    /// return modified copies of the original.
+    /// </summary>
+    /// <typeparam name="T">Type of items to be contained in the list.</typeparam>
     public interface IPersistentList<T> : IEnumerable<T>
     {
-        /**Returns the number of objects currently in the list.*/
+        /// <summary>
+        /// Returns the number of objects currently in the list.
+        /// </summary>
         int Count { get; }
-
-        /**Adds an object of type T to the List and returns it.
-         *
-         * @param o Object to be added.
-         * @return New list with the additional object added.*/
+        
+        /// <summary>
+        /// Adds an object of type T to the List and returns it.
+        /// </summary>
+        /// <param name="o">Object to be added.</param>
+        /// <returns>New list with the additional object added.</returns>
         IPersistentList<T> Cons(T o);
-
-        /**Returns the list with all elements removed.*/
+        
+        /// <summary>
+        /// Returns a new list with all elements removed.
+        /// </summary>
         IPersistentList<T> Empty();
 
-        /**Checks if this list is equivalent to another list.
-         * Equivalent means it has equal items at the same positions.
-         *
-         * @param list The other list to compare with.
-         * @return True if the two lists are equivalent, False otherwise.*/
+        /// <summary>
+        /// Checks if this list is equivalent to another list.
+        /// Equivalent means it has equal items at the same positions.
+        /// </summary>
+        /// <param name="list">The other list to compare with.</param>
+        /// <returns>True if the two lists are equivalent, False otherwise.</returns>
         bool Equiv(IPersistentList<T> list);
 
-        /**Returns the object which was last added to the list.*/
+        /// <summary>
+        /// Returns the object which was last added to the list.
+        /// </summary>
         T Peek();
 
-        /**Returns a list where the object which was added last has been removed.*/
+        /// <summary>
+        /// Returns a list where the object which was added last has been removed.
+        /// </summary>
         IPersistentList<T> Pop();
 
-        /**Returns a list where the item specified has been removed.
-         * If no item in the list is equal the the item specified the entire list
-         * is returned.
-         *
-         * @param item The item to be removed.
-         * @return The list with the item specified removed. */
+        /// <summary>
+        /// Returns a list where the item specified has been removed.
+        /// If no item in the list is equal the the item specified the entire list
+        /// is returned.
+        /// </summary>
+        /// <param name="item">The item to be removed.</param>
+        /// <returns>The list with the item specified removed.</returns>
         IPersistentList<T> Without(T item);
     }
 }
