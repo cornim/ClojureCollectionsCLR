081cd79accdc4486e65b9ed009e03770d560eb10
Cornelius Mund <cornelius.mund@d-fine.de>
2014-02-28 15:52:15 +0100

    Updated documentation in IPersistentMap to C# style.

--- ClojureCollectionsCLR/IPersistentMap.cs
+++ ClojureCollectionsCLR/IPersistentMap.cs
@@ -2,57 +2,74 @@
 
 namespace ClojureCollectionsCLR
 {
-    /**Interface for a persistent (immutable) map.
-    * All methods are guaranteed to leave the object itself unmodified and only
-    * return modified copies of the original.
-    *
-    * @author Dr. Cornelius Mund
-    *
-    * @param <K> Key type
-    * @param <V> Value type
-    */
+    /// <summary>
+    /// Interface for a persistent (immutable) map.
+    /// All methods are guaranteed to leave the object itself unmodified and only
+    /// return modified copies of the original.
+    /// </summary>
+    /// 
+    /// <typeparam name="TK">Key type</typeparam>
+    /// <typeparam name="TV">Value type</typeparam>
     public interface IPersistentMap<TK, TV> : IEnumerable<KeyValuePair<TK, TV>>
     {
-        /**Returns the number of objects currently in the map.*/
+        /// <summary>
+        /// Returns the number of objects currently in the map.
+        /// </summary>
         int Count { get; }
 
-        /**Adds MapEntry to the map.
-         *
-         * @param entry MapEntry to be added to the map.
-         * @return New map with the additional MapEntry added.*/
+        /// <summary>
+        /// Adds MapEntry to the map.
+        /// </summary>
+        /// 
+        /// <param name="entry"> MapEntry to be added to the map.</param>
+        /// <returns> New map with the additional MapEntry added.</returns>
         IPersistentMap<TK, TV> Cons(KeyValuePair<TK, TV> entry);
 
-        /**Returns the map with all elements removed.*/
+        /// <summary>
+        /// Returns the map with all elements removed.
+        /// </summary>
         IPersistentMap<TK, TV> Empty();
 
-        /**Checks if this map is equivalent to another map.
-         * Equivalent means it has the same number of entries and
-         * equal values under equal keys.
-         *
-         * @param map The other map to compare with.
-         * @return True if the two vectors are equivalent, False otherwise.*/
+        /// <summary>
+        /// Checks if this map is equivalent to another map.
+        ///  Equivalent means it has the same number of entries and
+        ///  equal values under equal keys.
+        /// </summary>
+        /// 
+        /// <param name="map"> The other map to compare with.</param>
+        /// <returns> True if the two vectors are equivalent, False otherwise.</returns>
         bool Equiv(IPersistentMap<TK, TV> map);
 
-        /**Contains true if the map contains key as a key, false otherwise.*/
+        /// <summary>
+        /// Contains true if the map contains key as a key, false otherwise.
+        /// </summary>
         bool ContainsKey(TK key);
 
-        /**Returns the map entry for key or throws a KeyNotFoundException if key
-         * is not found in the map.*/
+        /// <summary>
+        /// Returns the map entry for key or throws a KeyNotFoundException if key
+        /// is not found in the map.
+        /// </summary>
         KeyValuePair<TK, TV> EntryAt(TK key);
 
-        /**Returns the value for key or throws a KeyNotFound exception if the key
-         * is not found in the map.*/
+        /// <summary>
+        /// Returns the value for key or throws a KeyNotFound exception if the key
+        /// is not found in the map.
+        /// </summary>
         TV ValAt(TK key);
 
-        /**Returns the value for key or notFound if key is not found in the map.
-         *
-         * @param key Key for which the value should be returned.
-         * @param notFound Object which is returned if key is not found.*/
+        /// <summary>
+        /// Returns the value for key or notFound if key is not found in the map.
+        /// </summary>
+        /// 
+        /// <param name="key">Key for which the value should be returned.</param>
+        /// <param name="notFound">Object which is returned if key is not found.</param>
         TV ValAt(TK key, TV notFound);
 
-        /**Returns a new PersistentMap where the key value pair is added to the
-         * current map. If key was already present in the map the corresponding
-         * value is overwritten.*/
+        /// <summary>
+        /// Returns a new PersistentMap where the key value pair is added to the
+        /// current map. If key was already present in the map the corresponding
+        /// value is overwritten.
+        /// </summary>
         IPersistentMap<TK, TV> Assoc(TK key, TV value);
 
         /// <summary>
@@ -61,8 +78,10 @@ namespace ClojureCollectionsCLR
         /// </summary>
         IPersistentMap<TK, TV> AssocEx(TK key, TV value);
 
-        /**Returns the current map without key and the corresponding value. If key
-         * is not part of the map the entire map is returned.*/
+        /// <summary>
+        /// Returns the current map without key and the corresponding value. If key
+        /// is not part of the map the entire map is returned.
+        /// </summary>
         IPersistentMap<TK, TV> Without(TK key);
     }
 }
